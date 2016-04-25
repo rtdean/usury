9d210214b2cb07510e5454076098e21db837e6c2
Ryan T. Dean <rtdean@cytherianage.net>
2016-04-24 23:54:46 -0500

    update license to Apache v2

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version='0.1',
     description='',
     long_description='',
+    license='Apache 2.0',
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Topic :: Office/Business :: Financial',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
@@ -19,6 +20,10 @@ setup(
     keywords='usury interest',
 
     packages=find_packages(),
+    package_data={
+        '': ['LICENSE'],
+    },
+    include_package_data=True,
 
     install_requires=[],
     tests_require=[],
